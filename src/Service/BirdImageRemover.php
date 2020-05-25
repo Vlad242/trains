d3cbd2ec97fdb02872ad134d1bdc5f5b347844b5
Vladyslavk <vlad242@i.ua>
2020-05-26 00:58:03 +0300

    Add upload/remove image services

---
+++ src/Service/BirdImageRemover.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+class BirdImageRemover
+{
+    private $targetDirectory;
+
+    public function __construct($targetDirectory)
+    {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function remove($file)
+    {
+
+        try {
+            unlink($this->getTargetDirectory()."/".$file);
+        } catch (FileException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
+    }
+}
---
+++ src/Service/RegionImageUploader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class RegionImageUploader
+{
+    private $targetDirectory;
+
+    public function __construct($targetDirectory)
+    {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function upload(UploadedFile $file)
+    {
+        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getTargetDirectory(), $fileName);
+        } catch (FileException $e) {
+            var_dump($e->getCode(), $e->getMessage());
+            exit;
+        }
+
+        return $fileName;
+    }
+
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
+    }
+}
